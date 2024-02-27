a2d601783e2cabd5a8e6c1aadf056d4b563c1a59
Andres Järviste <andres.jarviste@wbscodingschool.com>
2024-02-27 19:41:12 +0200

    esimene loeng

---
+++ src/Luuletus.js
@@ -0,0 +1,12 @@
+function Luuletus() {
+	return (
+		<>
+			<p>Kes hommikuti külas käib,</p>
+			<p>see asjatult ei longi</p>
+			<p>tidilipomm</p>
+			<p>sest selleks hommik ongi!</p>
+		</>
+	);
+}
+
+export default Luuletus;
